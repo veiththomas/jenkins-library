c8d4101e74632a29f9b5cf96748e0244bb7c1db5
Thomas Veith <thomas.veith@sap.com>
2020-08-20 12:02:23 +0200

    deactivate deletion

--- vars/sonarExecuteScan.groovy
+++ vars/sonarExecuteScan.groovy
@@ -75,7 +75,7 @@ void call(Map parameters = [:]) {
                     }
                 }
             } finally {
-                def ignore = sh script: 'rm -rf .sonar-scanner .certificates', returnStatus: true
+                def ignore = sh script: 'echo "huhu"', returnStatus: true
             }
         }
     }
